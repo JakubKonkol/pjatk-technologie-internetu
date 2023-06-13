4924a7a8c4ada518ab5b3a97d02a7b6c66763918
JakubKonkol <s24406@pjwstk.edu.pl>
2023-06-13 12:20:22 +0200

    zajecia 10

---
+++ zajecia10-11/Auto.js
@@ -0,0 +1,12 @@
+export class Auto{
+    rok;
+    przebieg;
+    cena_wyjsciowa;
+    cena_koncowa;
+    constructor(rok, przebieg, cena_wyjsciowa){
+        this.rok = rok;
+        this.przebieg = przebieg;
+        this.cena_wyjsciowa = cena_wyjsciowa;
+        this.cena_koncowa = cena_wyjsciowa;
+    }
+}
---
+++ zajecia10-11/app.js
@@ -0,0 +1,64 @@
+import { Auto } from "./Auto.js";
+//methods for buttons 
+function btn1() {
+    let newChild = document.createElement("div");
+    newChild.classList.add("div");
+    document.body.appendChild(newChild);
+}
+function btn2() {
+    let divs = document.querySelectorAll(".div");
+    if(divs.length < 0){
+        throw new Error("Nie ma żadnych divów do usunięcia");
+    }
+    divs[0].remove();
+}
+function btn3() {
+    let divs = document.querySelectorAll(".div");
+    if(divs.length < 0 || divs.length < 3){
+        throw new Error("Nie ma zadnych divow albo jest ich mniej niz 3");
+    }
+    divs[2].style.backgroundColor = "red";
+}
+function btn4() {
+    let divs = document.querySelectorAll(".div");
+    divs.forEach((div) => {
+        div.textContent = "nowy tekst";
+    })
+}
+function calculate() {
+    let firstNumber = document.getElementById("first").value;
+    let secondNumber = document.getElementById("second").value;
+    let operation = document.getElementById("operation").value;
+    let resultDiv = document.getElementById("result");
+    if(firstNumber === "" || secondNumber === ""){
+        throw new Error("Nie podano liczb");
+    }
+    switch(operation){
+        case "dodawanie":
+            resultDiv.textContent = parseInt(firstNumber) + parseInt(secondNumber);
+            break;
+        case "odejmowanie":
+            resultDiv.textContent = parseInt(firstNumber) - parseInt(secondNumber);
+            break;
+        case "mnozenie":
+            resultDiv.textContent = parseInt(firstNumber) * parseInt(secondNumber);
+            break;
+        case "dzielenie":
+            if(secondNumber === "0"){
+                alert("Nie można dzielić przez 0");
+                throw new Error("Nie można dzielić przez 0");
+            }
+            resultDiv.textContent = parseInt(firstNumber) / parseInt(secondNumber);
+            break;
+        default:
+            throw new Error("Nieznana operacja");
+    }
+}
+
+let cars = [
+    new Auto(2000, 100000, 10000),
+    new Auto(2001, 200000, 20000),
+    new Auto(2002, 300000, 30000),
+    new Auto(2003, 400000, 40000),
+    new Auto(2004, 500000, 50000)
+]
